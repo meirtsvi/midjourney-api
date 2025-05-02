34057783637982741c46a72cec812f55aa7387a3
Meir Tsvi <meir.tsvi@live.com>
2025-05-02 07:34:05 +0300

    Fixed nonce issue

--- src/discord.ws.ts
+++ src/discord.ws.ts
@@ -78,7 +78,7 @@ export class WsMessage {
     return new Promise((resolve) => {
       this.once("ready", (user) => {
         //print user nickname
-        console.log(`🎊 ws ready!!! Hi: ${user.global_name}`);
+        console.log(`🎊 WebSocket ready!!! Hi: ${user.global_name}`);
         resolve(this);
       });
     });
@@ -176,7 +176,7 @@ export class WsMessage {
       }
     }
 
-    if (!nonce && attachments?.length > 0 && components?.length > 0) {
+    if (attachments?.length > 0 && components?.length > 0) {
       this.done(message);
       return;
     }
